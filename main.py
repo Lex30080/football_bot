5703d6650b4871213676d0df1bfe6166abcbe74f
Aleksei Novikov <green_dale@hotmail.com>
2026-05-17 19:06:57 +0300

    implemented lining up logic

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ dp = Dispatcher()
 
 MIN_PLAYERS = 10
 MAX_PLAYERS = 12
+game_active = False
+players_for_game = []
+
+# implementation of the game status
+def get_game_status():
+    if not game_active:
+        return "Нет активной игры."
+    elif len(players_for_game) < MIN_PLAYERS:
+        return f"Игроков {len(players_for_game)}\nДо игры нужно еще {MIN_PLAYERS - len(players_for_game)} игроков."
+    elif len(players_for_game) == MIN_PLAYERS:
+        return f"Игрков {len(players_for_game)}/{MAX_PLAYERS}\nМожно бронировать поле\nПродолжаем набор до {MAX_PLAYERS}"
+    elif len(players_for_game) < MAX_PLAYERS:
+        return f"Игроков {len(players_for_game)}/{MAX_PLAYERS}\nПродолжаем набор до {MAX_PLAYERS}"
+    else:
+        return f"Игроков {len(players_for_game)}/{MAX_PLAYERS}\nНабор окончен, можно играть!"
+
 
 # implementation of the start command
 @dp.message(CommandStart())
@@ -29,7 +45,7 @@ async def main():
     print("Bot started")
     await dp.start_polling(bot)
 
-players_for_game =[]
+
 
 # all availiable players
 players = [
@@ -89,25 +105,71 @@ async def teams_handler(message: Message):
 @dp.message(Command("game"))
 async def game_handler(message: Message):
     global players_for_game
-    players_for_game = []
-    await message.answer("Новая игра началась! нажми /join чтобы записаться")
+    global game_active
+    if not game_active:
+        game_active = True
+        players_for_game = []
+        await message.answer("Новая игра началась! нажми /join чтобы записаться")
+    else:
+        await message.answer(f"Набор уже идет! {len(players_for_game)}/{MAX_PLAYERS} игроков записано")    
+
 
 # implementation of the /join command
-
 @dp.message(Command("join"))
 async def join_handler(message: Message):
+   if not game_active:
+        await message.answer("❌ Сейчас нет активной игры.\nИспользуйте /game")
+        return
    if len(players_for_game) >= MAX_PLAYERS:
         await message.answer("Мест нет, 12/12")
         return 
    
-   name = message.from_user.first_name
+   parts = message.text.split()
+   name = parts[1]
    
    if name not in players_for_game:
        players_for_game.append(name)
-       await message.answer(f"{name} присоединился к игре! ({len(players_for_game)}/{MIN_PLAYERS})")
+       status = get_game_status()
+       await message.answer(status)
    else:
        await message.answer(f"{name} уже записан на игру.")
 
+# implememntation of the /lineup command
+@dp.message(Command("lineup"))
+async def lineup_handler(message: Message):
+    if not game_active:
+        await message.answer("Сейчас нет активной игры.\nИспользуйте /game")
+        return
+    await message.answer("Состав игроков:\n\n" + "\n".join(players_for_game))
 
+
+# implementaion of the /cancel command
+@dp.message(Command("cancel"))
+async def cancel_handler(message: Message):
+    global game_active
+    global players_for_game
+    if not game_active:
+        await message.answer("Сейчас нет активной игры.")
+        return
+    game_active = False
+    players_for_game = []
+    await message.answer("Игра отменена.")
+
+
+# implementation of the /leave command
+@dp.message(Command("leave"))
+async def leave_handler(message: Message):
+    global players_for_game
+    if not game_active:
+        await message.answer("Сейчас нет активной игры.")
+        return
+    parts = message.text.split()
+    name = parts[1]
+    if name in players_for_game:
+        players_for_game.remove(name)
+        status = get_game_status()
+        await message.answer(f"{name} покинул игру.\n\n{status}")
+    else:
+        await message.answer(f"{name} не записан на игру.")
 
 asyncio.run(main())
